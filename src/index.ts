07d420357c031f6ca842d0e4147447558a0f62e0
Pedro Melo <pedromarcosprado26@gmail.com>
2023-06-02 02:23:09 -0300

    att

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 const message: string = "Hello World Typescript"
-console.log(message)
+console.log(message);
+
+let testeUnknown: unknown;
+
+testeUnknown = 123;
+testeUnknown = 'Leite com pêra'
+console.log(testeUnknown);
